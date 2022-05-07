8af698a646ef9470352522d2cfe46e7bc8d589b4
sseq007 <sseq007@naver.com>
2022-05-07 20:45:45 +0900

    Update month of the Calendar

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -11,34 +11,20 @@ public class Calendar {
 		return max_Days[month-1];
 	}
 	
-	static void printSampleCalendar() {
-		System.out.println("일 월 화 수 목 금 토 일");
-		System.out.println("---------------------");
-		System.out.println(" 1  2  3  4  5  6  7");
-		System.out.println(" 8  9 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");
-	}
-	public static void main(String[] args) {
-
-
-		Scanner kb= new Scanner(System.in);
-		Calendar cal= new Calendar();
-		System.out.println("반복횟수를 입력하세요");
-		int repeat = kb.nextInt();
-		for(int i=0;i<repeat;i++) {
-			System.out.println("달을 입려하세요");
-			int month =kb.nextInt();
-			System.out.printf("%d월은 %d일 까지 있습니다.\n",month,cal.getMaxDaysofMonth(month));
-			cal.printSampleCalendar();
-		}
-		System.out.println("BYE");
-		kb.close();
-
+	static void printCalendar(int year,int month) {
+		System.out.printf("   <<%4d년%3d월>>\n",year,month);
+		System.out.println(" SU MO TU WE TH FI SA");
+		System.out.println("-----------------------");
 		
-
-
-
+		int maxDay=getMaxDaysofMonth(month);
+		for(int i=1;i<maxDay;i++) {
+			System.out.printf("%3d",i);
+			if(i%7==0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
 	}
+	
 
 }
---
+++ src/honux/calendar/Prompt.java
@@ -0,0 +1,35 @@
+package honux.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT="cal> ";
+	public static void runPrompt() {
+		String PROMPT="cal> ";
+		Scanner kb= new Scanner(System.in);
+		Calendar cal= new Calendar();
+		
+		while(true) {
+			System.out.println("달을 입려하세요");
+			System.out.print(PROMPT);
+			int month =kb.nextInt();
+			if(month==0) {
+				break;
+			}
+			if(month>12) {
+				continue;
+			}
+			
+			cal.printCalendar(2022,month);
+		}
+		System.out.println("BYE");
+		kb.close();
+		
+	}
+	public static void main(String[] args) {
+		Prompt p=new Prompt();
+		p.runPrompt();
+
+	}
+}
