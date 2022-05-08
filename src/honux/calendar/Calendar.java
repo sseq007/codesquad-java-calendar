a16c8c0614358cdf3354744fb5201e2de29eafdb
sseq007 <sseq007@naver.com>
2022-05-08 20:27:49 +0900

    ADD to caculate Days of Canlendar

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -4,8 +4,8 @@ package honux.calendar;
 
 public class Calendar {
 
-	private static final int [] max_Days= {31,28,31,30,31,30,31,31,30,31,30,31};
-	private static final int [] LEAP_MAX_DAYS = {31,29,31,30,31,30,31,31,30,31,30,31};
+	private static final int [] max_Days= {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	private static final int [] LEAP_MAX_DAYS = {0,31,29,31,30,31,30,31,31,30,31,30,31};
 
 	public static boolean isLeapYear(int year) {
 
@@ -18,17 +18,18 @@ public class Calendar {
 	}
 	public static int getMaxDaysofMonth(int year,int month) {
 		if(isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month-1];
+			return LEAP_MAX_DAYS[month];
 		}else {
-			return max_Days[month-1];
+			return max_Days[month];
 		}
 	}
 	
-	void printCalendar(int year,int month,int weekDay) {
-		System.out.printf("   <<%4d년%3d월>>\n",year,month);
+	void printCalendar(int year,int month) {
+		System.out.printf("   <<%d년 %d월>>\n",year,month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("-----------------------");
 		
+		int weekDay = getWeekDay(year, month, 1);
 		int	maxDay=getMaxDaysofMonth(year,month);
 		int count=7-weekDay;
 		int delim = (count<7)?count:0;
@@ -50,5 +51,34 @@ public class Calendar {
 		System.out.println();
 	}
 	
+	int getWeekDay(int year,int month,int day) {
+		int syear=1970;
+		final int STANDARD_WEEKDAY=3;
+		
+		int count=0;
+		for(int i=syear;i<year;i++) {
+			int delta = isLeapYear(i)?366:365;
+			count+=delta;
+		}
+		for(int i=1;i<month;i++) {
+			int delta = getMaxDaysofMonth(year, i);
+			
+			count+=delta;
+		}
+		count+=day;
+		int weekDay= (count + STANDARD_WEEKDAY) %7;
+		
+		
+		return weekDay;
+		
+	}
+	
+	//simple test code
+	public static void main(String[] args) {
+		Calendar cal =new Calendar();
+		cal.getWeekDay(1970, 1, 1);
+		cal.getWeekDay(1971, 1, 1);
+		cal.getWeekDay(1972, 1, 1);
+	}
 
 }
--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -4,16 +4,7 @@ import java.util.Scanner;
 
 public class Prompt {
 
-	public int parseDay(String week) {
-		if(week.equals("su")) return 0;
-		else if(week.equals("mo")) return 1;
-		else if(week.equals("tu")) return 2;
-		else if(week.equals("we")) return 3;
-		else if(week.equals("th")) return 4;
-		else if(week.equals("fr")) return 5;
-		else if(week.equals("sa")) return 6;
-		else return 0;
-	}
+
 	
 	public void runPrompt() {
 		
@@ -30,10 +21,7 @@ public class Prompt {
 			System.out.println("달을 입려하세요");
 			System.out.print("MONTH");
 			int month =kb.nextInt();
-			System.out.println("첫째 날의 요일을 입력하세요");
-			System.out.print("WEEKDAY");
-			String week =kb.next();
-			int weekDay=parseDay(week);
+		
 			
 			
 			if(month>12 || month<1) {
@@ -41,7 +29,7 @@ public class Prompt {
 				continue;
 			}
 			
-			cal.printCalendar(year,month,weekDay);
+			cal.printCalendar(year,month);
 		}
 		System.out.println("BYE");
 		kb.close();
